8b90228d6bbcad7bba02a569529cfeab4e7e7f0e
vivek yadav <smartvivekyadav9@gmail.com>
2023-04-15 16:58:46 +0530

    adding content to homepage

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
-const homeRouter = require("./views/homepage/server")
+const homepageRouter = require("./views/homepage/server")
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 
-app.use("/", homeRouter)
+app.use("/", homepageRouter)
 
 app.listen(PORT, ()=>{
     console.log(`server is live at ${PORT}`);
