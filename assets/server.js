3484d0c4cd9a5c1d66b30235d9f8cc31f8354e1c
vivek yadav <smartvivekyadav9@gmail.com>
2023-04-16 17:59:56 +0530

    added wiki to homepage

--- assets/server.js
+++ assets/server.js
@@ -5,4 +5,13 @@ router.get("/bionic-script-logo.png", (req, res)=>{
     res.sendFile(`${__dirname}/bionic-script-logo.png`)
 })
 
+router.get("/bionic-script-logo.ico", (req, res)=>{
+    res.sendFile(`${__dirname}/bionic-script-logo.ico`)
+
+})
+
+router.get("/wikipedia-icon.png", (req, res)=>{
+    res.sendFile(`${__dirname}/wikipedia-icon.png`)
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const assetsRouter = require('./assets/server')
 const homepageRouter = require("./views/homepage/server")
 const bionicRouter = require("./views/bionic/server")
 
+app.set('view engine', 'hbs');
+
 const PORT = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({ extended: true }))
